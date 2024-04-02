b05ff3dd234a1cf77ffafd0f4631ec7ecaa5c73c
Abdul Zia <52299063+abdulziaa@users.noreply.github.com>
2024-04-02 06:30:25 -0400

    fastapi (actually being used)

--- server.py
+++ server.py
@@ -1,16 +1,14 @@
-# from fastapi import FastAPI, Query
-# import sys
-# import os
-# import time
+from fastapi import FastAPI, Query
+import sys
+import os
+import time
 import subprocess
 
-# app = FastAPI()
+app = FastAPI()
 
 defaultsound = "/home/abdul/PiFmRds/src/sound.wav"
 freq = "89.7"
 
+@app.get("/play")
 def start():
     subprocess.run(["sudo", "/home/abdul/PiFmRds/src/pi_fm_rds", "-freq", freq, "-audio", defaultsound])
-    
-start()
-    
