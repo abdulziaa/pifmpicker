97a792a0dcf10e6190b482f686b3c8d0696bdc35
Abdul Zia <52299063+abdulziaa@users.noreply.github.com>
2024-04-02 06:46:20 -0400

    fixes!

--- server.py
+++ server.py
@@ -11,4 +11,5 @@ freq = "89.7"
 
 @app.get("/play")
 def start():
-    subprocess.run(["sudo", "/home/abdul/PiFmRds/src/pi_fm_rds", "-freq", freq, "-audio", defaultsound])
+    subprocess.run(["sudo", "/home/abdul/PiFmRds/src/pi_fm_rds", "-freq", freq, "-audio", defaultsound], Shell=True)
+    return f"Playing {defaultsound}"
